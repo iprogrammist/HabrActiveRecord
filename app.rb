46c83726aec78c990227a0d5bc9a10d4a019dc19
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-16 01:11:05 +0300

    Homework Lesson 30

--- app.rb
+++ app.rb
@@ -1,8 +1,61 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "habr.db"}
+
+class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :author, presence: true, length: { minimum: 3 }
+	validates :content, presence: true
+end
+
+class Comment < ActiveRecord::Base
+	belongs_to :post
+	validates :comment_content, presence: true
+end
+
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@results = Post.order('created_at DESC')
+	erb :index			
+end
+
+get '/new' do
+	@p = Post.new
+	erb :new			
+end
+
+post '/new' do
+	@p = Post.new params[:post]
+	if @p.save
+		redirect to '/'
+	else
+		@error = @p.errors.full_messages.first
+		erb :new
+	end			
+end
+
+get '/details/:post_id' do
+	@post = Post.find(params[:post_id])
+
+	@comments = @post.comments
+  
+  	erb :details		
+end
+
+post '/details/:post_id' do
+  @post = Post.find(params[:post_id])
+
+  @comment = Comment.new(params[:comment])
+  @comment.post = @post
+  if @comment.save
+  	@comments = @post.comments
+  	erb :details
+  else
+  	@error = @comment.errors.full_messages.first
+  	@comments = @post.comments
+  	erb :details
+  end	
 end
